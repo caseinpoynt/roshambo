de01913b3ca82bdc7ee3f6ba2a7c1f5423def265
caseinpoynt <case.nelson@poynt.com>
2012-04-27 21:01:09 -0600

    Update rock.py

--- rock.py
+++ rock.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python -u
 
-import sys
-
-moves = []
 while True:
     # good old rock, nothing beats that
     print "rock"
     move = raw_input()
     if move == "goodbye":
         break
-    moves.append(move)
 
