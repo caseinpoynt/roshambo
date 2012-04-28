2d3ef2c74f6a90dff210cf92ec21e7b7fc26bfca
Case Nelson <case@teammating.com>
2012-04-27 20:17:18 -0600

    simplifying examples

--- paper.py
+++ paper.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python -u
 
-import sys
-
-moves = []
 while True:
     print "paper"
     move = raw_input()
     if move == "goodbye":
         break
-    moves.append(move)
--- rock.py
+++ rock.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python -u
 
-import sys
-
-moves = []
 while True:
     print "rock"
     move = raw_input()
     if move == "goodbye":
         break
-    moves.append(move)
 
