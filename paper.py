a03377dd4e439047a09a620f5e93447e08e8335f
caseinpoynt <case.nelson@poynt.com>
2012-04-27 21:00:32 -0600

    Update paper.py

--- paper.py
+++ paper.py
@@ -2,11 +2,9 @@
 
 import sys
 
-moves = []
 while True:
     # poor predictable Bart; always takes rock 
     print "paper"
     move = raw_input()
     if move == "goodbye":
         break
-    moves.append(move)
